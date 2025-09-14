ce5c58b63cc69d6db8b6cc3dd13713fc73bde767
Victor <victormovileanu@gmail.com>
2025-09-14 16:21:07 +0200

    (pwa-workshop) File System API and more

---
+++ pwa-workshop-codelab-pwa03-workbox/js/preview.js
@@ -0,0 +1,9 @@
+import {openDB} from "idb";
+import {marked} from "marked";
+
+window.addEventListener("DOMContentLoaded", async (event) => {
+  const preview = document.querySelector(".preview");
+  const db = await openDB('settings-store')
+  const content = (await db.get('settings', 'content') || '')
+  preview.innerText = marked(content)
+})
