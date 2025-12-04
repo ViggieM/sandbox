9a8887874da626ee468b78ba17510fe86532d6a0
Victor <victormovileanu@gmail.com>
2025-12-04 12:40:51 +0100

    WIP

--- vitePWA+sveltekit/src/lib/ReloadPrompt.svelte
+++ vitePWA+sveltekit/src/lib/ReloadPrompt.svelte
@@ -1,16 +1,33 @@
 <script lang="ts">
     import { useRegisterSW } from "virtual:pwa-register/svelte";
+    import { dev } from "$app/environment";
+
+    const intervalMS = 60 * 60 * 1000;
     const { needRefresh, updateServiceWorker, offlineReady } = useRegisterSW({
-        onRegistered(r) {
-            // uncomment following code if you want check for updates
-            // r && setInterval(() => {
-            //   console.log('Checking for sw update')
-            //   r.update()
-            // }, 20000 /* 20s for testing purposes */)
-            console.log(`SW Registered: ${r}`);
+        onRegisteredSW(swScriptUrl, registration) {
+            console.log(`SW Registered: ${registration}`);
+
+            /* Periodically check for updates */
+            if (registration) {
+                setInterval(
+                    () => {
+                        console.log("SW Checking for updates");
+                        registration.update();
+                    },
+                    dev ? 3000 : intervalMS,
+                );
+            }
         },
         onRegisterError(error) {
             console.log("SW registration error", error);
+        },
+        onNeedRefresh() {
+            console.log(`SW needs refresh`);
+        },
+        onOfflineReady() {
+            console.log(
+                `SW was installed and is now ready to serve offline requests`,
+            );
         },
     });
     const close = () => {
--- vitePWA+sveltekit/src/routes/+layout.svelte
+++ vitePWA+sveltekit/src/routes/+layout.svelte
@@ -1,23 +1,10 @@
 <script lang="ts">
     import favicon from "$lib/assets/favicon.svg";
     import { pwaInfo } from "virtual:pwa-info";
-    import { onMount } from "svelte";
-    import { browser } from "$app/environment";
 
     const webManifest = $derived(pwaInfo ? pwaInfo.webManifest.linkTag : "");
 
     let { children } = $props();
-
-    onMount(async () => {
-        /**
-         * If you are using SSR in SvelteKit, you need to check whether you are
-         * inside a browser context before registering the service worker.
-         * Reference: https://vite-pwa-org.netlify.app/guide/auto-update.html#ssr-ssg.
-         * */
-        if (pwaInfo && browser) {
-            // await import("./registerSW");
-        }
-    });
 </script>
 
 <svelte:head>
